0d04d47de00ea4d9a9898748e5abd2c307108779
Matt Dufort <matt.dufort@gmail.com>
2017-04-27 14:19:25 -0700

    Added functions for standardizing data frames and matrices.

---
+++ R/remove_all_NA_rowcols.R
@@ -0,0 +1,28 @@
+#' Trim any rows and/or columns containing all NAs from a matrix or data frame.
+#'
+#' Trim any rows and/or columns containing all NAs from a matrix or data frame. Any rows or columns
+#' composed entirely of NAs are removed.This is used primarily after reading in a spreadsheet, when excess
+#' columns and/or rows may be included due to quirks in the file reading process. It can also remove cases
+#' and/or variables with no data.
+#' @param x data frame or matrix with rows and/or columns to be removed
+#' @param cols,rows logical, whether to trim columns and rows, respectively. By default, both columns and rows are trimmed.
+#' @export
+#' @return an object of the same type as \code{x}, potentially with fewer rows and/or columns
+#' @usage \code{standardize_names(x, cols=TRUE, rows=TRUE, dedup=TRUE)}
+standardize_dimnames <- function(x, cols=TRUE, rows=TRUE, dedup=TRUE) {
+  if (!is.data.frame(x) & !is.matrix(x))
+    stop("Input object type not recognized. This function is for use on data frames and matrices.")
+  if (cols) {
+    keep_cols <-
+      apply(x, MARGIN=2, function(x) {sum(!is.na(x))}) > 0
+    x <- x[,keep_cols]
+  }
+  
+  if (rows) {
+    keep_rows <-
+      apply(x, MARGIN=1, function(x) {sum(!is.na(x))}) > 0
+    x <- x[,keep_rows]
+  }
+  
+  x
+}
---
+++ R/standardize_dimnames.R
@@ -0,0 +1,23 @@
+#' Standardize the column and/or row names of a matrix or data frame to standard syntax
+#'
+#' Standardize column and/or row names of a matrix or data frame to a standard syntax. This is a wrapper for
+#' \code{standardize_names}, to allow it to be used on data frames and matrices, rather than on their
+#' names. All letters are converted to lowercase, special characters are converted to underscores,
+#' and multiple and trailing (but not leading) special characters are removed. By default, elements are
+#' deduplicated by appending "_1" and so on.
+#' @param x data frame or matrix with column and/or row names to be standardized
+#' @param cols,rows logical, whether to standardize the column and row names, respectively. By default, column names are standardized, but row names are not.
+#' @param dedup logical, whether to deduplicate the vector elements. If TRUE, elements that are identical after standardization are deduplicated by appending "_1", "_2", etc. Defaults to TRUE.
+#' @import stringr
+#' @export
+#' @return an object of the same type and dimension as \code{x}
+#' @usage \code{standardize_names(x, cols=TRUE, rows=FALSE, dedup=TRUE)}
+standardize_dimnames <- function(x, cols=TRUE, rows=FALSE, dedup=TRUE) {
+  if (!is.data.frame(x) & !is.matrix(x))
+    stop("Input object type not recognized. This function is for use on data frames and matrices.")
+  if (cols)
+    colnames(x) <- standardize_names(colnames(x), dedup=dedup)
+  if (rows)
+    rownames(x) <- standardize_names(rownames(x), dedup=dedup)
+  x
+}
