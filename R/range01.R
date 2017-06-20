ecb69e74565f1a32aeba19cd80abf918839f0a25
Matt Dufort <matt.dufort@gmail.com>
2017-06-20 11:28:15 -0700

    Add NA handling to range01. Update roxygen version.

--- R/range01.R
+++ R/range01.R
@@ -1,5 +1,8 @@
 #' Normalize a numeric vector to 0-1 range
+#' 
+#' This function normalizes a numeric vector to have minimum of 0 and maximum of 1, with values in between linearly
+#' transformed. It ignores NA values in the calculation of min and max.
 #' @param x numeric vector to be normalized
 #' @export
 #' @usage \code{range01(x)}
-range01 <- function(x)((x-min(x))/(max(x)-min(x)))
+range01 <- function(x)((x-min(x, na.rm=TRUE))/(max(x, na.rm=TRUE)-min(x, na.rm=TRUE)))
