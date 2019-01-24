7773106ad383a054ac3452a9e2fba9cf2ceecb25
mjdufort <matt.dufort@gmail.com>
2019-01-24 09:08:39 -0800

    Update is_outlier to handle NAs

--- R/is_outlier.R
+++ R/is_outlier.R
@@ -2,10 +2,10 @@
 #' 
 #' Determine outliers of a numeric vector, based on the first and third quartile minus/plus 1.5 times the
 #' inter-quartile range.
-#' @param x numeric vector containing values test for outliers
+#' @param x numeric vector containing values to test for outliers
 #' @export
-#' @return a boolean vector, containing TRUE for outlier elements and FALSE for all other elements.
-#' @usage \code{is_outlier(x)}
+#' @return logical vector, containing TRUE for outlier elements and FALSE for all other elements.
+#' @usage \code{is_outlier(x, na.rm=FALSE)}
 is_outlier <- function(x) {
-  (x < quantile(x, 0.25) - 1.5 * IQR(x)) | (x > quantile(x, 0.75) + 1.5 * IQR(x))
+  (x < quantile(x, 0.25, na.rm=na.rm) - 1.5 * IQR(x, na.rm=na.rm)) | (x > quantile(x, 0.75, na.rm=na.rm) + 1.5 * IQR(x, na.rm=na.rm))
 }
