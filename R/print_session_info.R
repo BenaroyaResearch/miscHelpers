a4e2c03e7202f681663d2f922498a3cd7b47219b
mjdufort <matt.dufort@gmail.com>
2020-03-13 10:22:21 -0700

    Update print_session_info to use package::function

--- R/print_session_info.R
+++ R/print_session_info.R
@@ -3,7 +3,7 @@
 #' Print the session information. This function is a wrapper for \code{sessioninfo::session_info()}, with
 #' the list of packages formatted using \code{kableExtra::kable_styling()} to make it more adaptive to
 #' the output context. In particular, this function works much better with knitr output to html or pdf.
-#' In its current state, there are no parameters, and only default settings. Additional flexibility, 
+#' In its current state, there are no parameters, and only default settings. Additional flexibility,
 #' such as controlling the fields that are output, may be added in the future.
 #' @export
 #' @usage print_session_info()
@@ -11,7 +11,7 @@ print_session_info <- function() {
   ses_info <- sessioninfo::session_info()
   print(ses_info$platform)
   kableExtra::kable_styling(
-    kable(
+    knitr::kable(
       dplyr::select(ses_info$packages, package, version=loadedversion, date, source),
       row.names=FALSE))
 }
