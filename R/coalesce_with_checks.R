e760e844d6c6395de1714de1b7b051c9a4f047f0
mjdufort <matt.dufort@gmail.com>
2020-03-13 17:35:46 -0700

    Update coalesce_with_checks to handle numerical similarity

--- R/coalesce_with_checks.R
+++ R/coalesce_with_checks.R
@@ -8,29 +8,31 @@
 #' more of the input columns, and places that value into the first column.
 #' @param x data frame containing the redundant columns to be coalesced/collapsed
 #' @param columns character vector, the names of the columns to be coalesced/collapsed. The values will be combined into the first column listed in \code{columns}, and the remaining columns will be dropped.
+#' @param tol numeric, the tolerance for assuming identity of floating-point numeric values. Defaults to the square route of the machine tolerance
 #' @export
 #' @return A data frame with \code{columns} values collapsed into the first column listed.
 #' @usage coalesce_with_checks(x, columns)
 
-coalesce_with_checks <- function(x, columns) {
+coalesce_with_checks <- function(x, columns, tol=.Machine$double.eps^0.5) {
   if (sum(columns %in% colnames(x)) < 2) # check for at least 2 columns in x
     stop(
       "Function coalesce_with_checks requires at least two valid columns in input data frame")
-  if (any(apply(x[,columns], MARGIN=1, function(x) length(unique(na.omit(x)))) > 1))
+
+  if (any(apply(x[,columns], MARGIN=1,
+                function(y) {!(all(is.na(unlist(y))) | all_equal(na.omit(unlist(y)), tol=tol))})))
     stop(
       paste(
-        "Multiple non-NA values found in at least one row of annotation for the following columns:",
-        paste(columns, collapse=", ")))
-  
-  columns_to_coalesce <- intersect(columns, colnames(x))
-  if (!setequal(columns_to_coalesce, columns))
-    warning("Some columns input to coalesce_with_checks were not found in input data object.")
-  
+          "Multiple non-NA values found in at least one row of annotation for the following columns:",
+          paste(columns, collapse=", ")))
+    columns_to_coalesce <- intersect(columns, colnames(x))
+    if (!setequal(columns_to_coalesce, columns))
+      warning("Some columns input to coalesce_with_checks were not found in input data object.")
+
   for (col.tmp in 2:length(columns)) {
     rows.tmp <- which(is.na(x[[columns[1]]]) & !is.na(x[[columns[col.tmp]]]))
     x[rows.tmp, columns[1]] <- x[rows.tmp, columns[col.tmp]]
     x[,columns[col.tmp]] <- NULL
   }
-  
+
   return(x)
 }
