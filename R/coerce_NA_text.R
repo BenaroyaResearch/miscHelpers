68fe274ff4e8acfc22d6bc0f3fd1a3a4b491ac9f
mjdufort <matt.dufort@gmail.com>
2020-03-17 11:38:40 -0700

    Add flexibility to coerce_NA_text

--- R/coerce_NA_text.R
+++ R/coerce_NA_text.R
@@ -5,12 +5,14 @@
 #' The values that will be coerced to NAs can be provided as a character vector.
 #' @param x vector, matrix, or data frame containing the data to be coerced
 #' @param NA_values character vector, the values to be coerced to NAs.
+#' @param add_NA_values character vector, additional values (beyond \code{NA_values}) to coerce to NA. This argument simplifies keeping the default \code{NA_values} and adding one or more additional values to it
 #' @param verbose logical, whether to output information on data being coerced; if \code{x} is a data frame, the function will output the names of the columns with data being coerced.
 #' @export
 #' @return An object of the same type as the input data
-#' @usage coerce_NA_text(x, NA_values=c("NA", "na", "N/A", "n/a", "", "."), verbose=FALSE)
+#' @usage coerce_NA_text(x, NA_values=c("NA", "na", "N/A", "n/a", "", "."), add_NA_values, verbose=FALSE)
 
-coerce_NA_text <- function(x, NA_values=c("NA", "na", "N/A", "n/a", "", "."), verbose=FALSE) {
+coerce_NA_text <- function(x, NA_values=c("NA", "na", "N/A", "n/a", "", "."), add_NA_values=NULL, verbose=FALSE) {
+  if (!is.null(add_NA_values)) NA_values <- union(NA_values, add_NA_values)
   if (is.vector(x) | is.matrix(x)) {
     x[x %in% NA_values] <- NA
   } else if (inherits(x, "data.frame")) {
